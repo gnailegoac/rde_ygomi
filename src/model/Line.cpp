cac72a1b01ff62fb57918e4bce38738e649f01a8
yangqiang200608 <476399732@qq.com>
2018-02-26 15:21:04 +0800

    RED-112: let simpling caculation be later of data load

--- src/model/Line.cpp
+++ src/model/Line.cpp
@@ -206,10 +206,10 @@ void Model::Line::GenerateViewPaintMap(std::unique_ptr<CRS::ICoordinateTransform
 {
     // map viewer needs level 1-5 data.
     mPaintListMap->insert(std::make_pair(1, std::make_shared<PaintList>()));
-    //mPaintListMap->insert(std::make_pair(2, std::make_shared<PaintList>()));
-    //mPaintListMap->insert(std::make_pair(3, std::make_shared<PaintList>()));
-    //mPaintListMap->insert(std::make_pair(4, std::make_shared<PaintList>()));
-    //mPaintListMap->insert(std::make_pair(5, std::make_shared<PaintList>()));
+    mPaintListMap->insert(std::make_pair(2, std::make_shared<PaintList>()));
+    mPaintListMap->insert(std::make_pair(3, std::make_shared<PaintList>()));
+    mPaintListMap->insert(std::make_pair(4, std::make_shared<PaintList>()));
+    mPaintListMap->insert(std::make_pair(5, std::make_shared<PaintList>()));
 
     for (Point3DListPtr& geodeticPoints : *mGeodeticPointsList)
     {
@@ -227,10 +227,10 @@ void Model::Line::GenerateViewPaintMap(std::unique_ptr<CRS::ICoordinateTransform
 
         // Down-sample points with Douglas-Peucker algorithm
         mPaintListMap->at(1)->push_back(Model::DouglasPeucker::SimplifyS(points, 2));
-        //mPaintListMap->at(2)->push_back(Model::DouglasPeucker::Simplify(points, 1));
-        //mPaintListMap->at(3)->push_back(Model::DouglasPeucker::Simplify(points, 0.5));
-        //mPaintListMap->at(4)->push_back(Model::DouglasPeucker::Simplify(points, 0.2));
-        //mPaintListMap->at(5)->push_back(Model::DouglasPeucker::Simplify(points, 0.1));
+        mPaintListMap->at(2)->push_back(Model::DouglasPeucker::Simplify(points, 1));
+        mPaintListMap->at(3)->push_back(Model::DouglasPeucker::Simplify(points, 0.5));
+        mPaintListMap->at(4)->push_back(Model::DouglasPeucker::Simplify(points, 0.2));
+        mPaintListMap->at(5)->push_back(Model::DouglasPeucker::Simplify(points, 0.1));
     }
 }
 
