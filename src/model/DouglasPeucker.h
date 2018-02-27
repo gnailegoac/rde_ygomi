70f82103d80ce04b8b4de34e4d9b9535ec9cfc46
yangqiang200608 <476399732@qq.com>
2018-02-27 10:38:59 +0800

    RDE-112: add  comments to DouglasPeuker.h

--- src/model/DouglasPeucker.h
+++ src/model/DouglasPeucker.h
@@ -30,9 +30,12 @@ public:
                     const double& aThreshold,
                     const std::uint32_t& aStartIndex,
                     const std::uint32_t& aEndIndex);
-
+    /*Both function Simplify and SimplifyS implement the same function，
+     but the only difference of them is that the latter use the square of distance in calculation.*/
     static Point3DListPtr SimplifyS(const Point3DListPtr& aPoints,
                                     const double& aThreshold);
+    /*Both function GetSimplifyIndex and GetSimplifyIndexS implement the same function，
+     but the only difference of them is that the 'S' means using the square of distance in calculation.*/
     static std::shared_ptr<std::vector<uint32_t> > GetSimplifyIndexS(
                     const Point3DListPtr& aPoints,
                     const double& aThreshold,
@@ -61,6 +64,8 @@ private:
                                             const Point3DPtr& aL1,
                                             const Point3DPtr& aL2);
     static double modSquare(const Point3DPtr& aVector);
+    /*Both function getFarthestPoint and getFarthestPointS implement the same function，
+     but the only difference of them is that the 'S' means using the square of distance in calculation.*/
     static std::pair<std::uint32_t, double> getFarthestPointS(
                     const Point3DListPtr& aPoints,
                     std::uint32_t aBegin,
